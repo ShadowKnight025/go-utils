b3ea90d2211a7516bc07d0cbcda9457ae4f1a4b0
ShadowKnight025 <tdog2441@gmail.com>
2025-04-20 18:20:31 -0500

    mod fixes

--- redis.go
+++ redis.go
@@ -8,24 +8,23 @@ import (
 
 )
 
-func _init_db(){
-
-	username, password, host,  err := file.read("~/.creds/rdb-general-creds.go")
-	if err != nil{
-		fmt.Println("Error reading DB creds file: ", err)
-	}
-	return username, password
-}
+//func _init_db(){
+//	username, password, host,  err := file.read("~/.creds/rdb-general-creds.go")
+//	if err != nil{
+//		fmt.Println("Error reading DB creds file: ", err)
+//	}
+//	return username, password
+//}
 
 func new_client(conn_str string) {
 
 	_CONN_STR := os.Getenv("redcons")
 
 	// redis://<user>:<pass>@localhost:6379/<db>
-	if _CONN_STR == nil{
-		username, password, host = _init_db()
-		_CONN_STR = fmt.Sprintf("redis://%s:%s@%s", username, password, host)
-	}
+	//if _CONN_STR == nil{
+	//	username, password, host = _init_db()
+	//	_CONN_STR = fmt.Sprintf("redis://%s:%s@%s", username, password, host)
+	//}
 	opt, err := redis.ParseURL(_CONN_STR)
 	if err != nil {
 		fmt.Println("Error creating DB Client: ", err)
