5afa4042a78c5d128cfc81fc2864eefdf29f03cd
ShadowKnight025 <tdog2441@gmail.com>
2025-04-20 17:36:40 -0500

    Adding correct path for go modules

---
+++ github.com/ShadowKnight025/go-utils/record.go
@@ -0,0 +1,9 @@
+package utils
+
+import(
+	"log"
+)
+
+type Record struct{
+	Data map[string]map[string]string
+}
---
+++ github.com/ShadowKnight025/go-utils/redis.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"os"
+	"fmt"
+	"context"
+	"github.com/redis/go-redis/v9"
+	"utils/file"
+)
+
+_CONN_STR := os.Getenv("redcons") or ""
+
+func _init_db(){
+
+	username, password, host,  err := file.read("~/.creds/rdb-general-creds.go")
+	if err != nil{
+		fmt.Println("Error reading DB creds file: ", err)
+	}
+	return username, password
+}
+
+func new_client(conn_str string) {
+
+	// redis://<user>:<pass>@localhost:6379/<db>
+	if _CONN_STR == nil{
+		username, password, host = _init_db()
+		_CONN_STR = fmt.Sprintf("redis://%s:%s@%s", username, password, host)
+	}
+	opt, err := redis.ParseURL(_CONN_STR)
+	if err != nil {
+		fmt.Println("Error creating DB Client: ", err)
+		os.Exit(1)
+	}
+
+	client := redis.NewClient(opt)
+	ctx    := context.Background()
+	return client, ctx
+}
+
